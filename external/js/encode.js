b1e6a40a92dc8f617a18b42ca38f9c6586eade7b
susheel kumar <techiesusheel@gmail.com>
2026-09-06 22:33:36 +0530

    .

--- external/js/encode.js
+++ external/js/encode.js
@@ -1,6 +1,6 @@
 const paths = [
-    "L2Fzc2V0cy9hcGkvZ2xvYmFsLmpz",
-    
+    "https://ha-api.netlify.app/assets/js/global.js",
+    "/external/js/security/content-protect.js",
     atob("L2Fzc2V0cy9hcGkvZ2xvYmFsLmpz")
 ];
 
